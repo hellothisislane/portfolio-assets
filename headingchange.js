218777e8ad6e1f57e9fb5c81a4aa27e86c48e074
hellothisislane <74877203+hellothisislane@users.noreply.github.com>
2020-11-22 15:08:15 -0500

    Update headingchange.js

--- headingchange.js
+++ headingchange.js
@@ -3,7 +3,7 @@ $(function () {
   wordsArray = ["an Illustrator", "an enigma", "a UX Designer", "not like the other girls"]; 
   setInterval(function () { 
     count++; 
-    $("#word").fadeOut(0, function () { 
+    $(".word").fadeOut(0, function () { 
       $(this).text(wordsArray[count % wordsArray.length]).fadeIn(0); 
     }); 
   }, 1000); 
